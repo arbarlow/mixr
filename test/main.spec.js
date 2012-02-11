92a10f66de7ac642cc3e122be5a2f97b7c41566f
Alex Barlow <alexbarlowis@gmail.com>
2012-02-11 18:52:08 +0000

    Add express intergration and base file processing:

--- test/main.spec.js
+++ test/main.spec.js
@@ -17,11 +17,9 @@ describe("Mixr", function() {
     fs.readFile('./test/assets/css/app.css', function(err, file){
       var result = Mixr.getRequireHeaders(file.toString('utf-8'));
       expect(result).toEqual([ 
-        '*= require reset.css', 
         '*= require bootstrap.min.css', 
-        '*= require defaults.css.less', 
-        '*= require forms.css',
-        '*= require_dir other' ])
+        '*= require main.css',
+        '*= require main.css.less' ])
       done();
     });
   });
@@ -42,40 +40,31 @@ describe("Mixr", function() {
   });
   
   it("Should be able to require a normal file", function(done) {
-    Mixr.require("main.css", function(output, err) {
+    Mixr.require("main.css", function(err, output) {
       expect(output).toBe("body{background-color: #000;}");
       done(err);
     });
   });
   
   it("Should be able to require a normal file with a local ./", function(done) {
-    Mixr.require("./main.css", function(output, err) {
+    Mixr.require("./main.css", function(err, output) {
       expect(output).toBe("body{background-color: #000;}");
       done(err);
     });
   });
   
   it("Should be able to require and compile a less file", function(done) {
-    Mixr.require("./main.css.less", function(output, err) {
+    Mixr.require("./main.css.less", function(err, output) {
       expect(output).toBe("#header {\n  color: #4d926f;\n}\n");
       done(err);
     });
   });
   
   it("Should be able to require and compile a coffeescript file", function(done) {
-    Mixr.require("./main.js.coffee", function(output, err) {
+    Mixr.require("./main.js.coffee", function(err, output) {
       expect(output).toBe("(function() {\n  var number, opposite;\n\n  number = 42;\n\n  opposite = true;\n\n  if (opposite) number = -42;\n\n}).call(this);\n");
       done(err);
     });
-  });
-  
-  it("Should output one whole file if given an array of files", function(done) {
-    var files = ['./test/assets/concat_test/1', './test/assets/concat_test/2', './test/assets/concat_test/3'];
-    Mixr.read_and_concatenate_files(files, function(output, err) {
-      expect(err).toBe(null);
-      expect(output).toBe('1\n2\n3\n');
-      done();
-    })
   });
   
 });
